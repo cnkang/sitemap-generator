f6b6b511ec805de1bb14313099fe55e80a8e8456
Kang Liu <555836+cnkang@users.noreply.github.com>
2023-07-04 19:46:47 +0800

    Update sitemap_generator.py
    optimize code to make pylint happy

--- sitemap_generator.py
+++ sitemap_generator.py
@@ -1,21 +1,26 @@
-import requests
-from bs4 import BeautifulSoup
+"""
+A script to crawl a website and generate a sitemap in XML format.
+"""
+
 from datetime import datetime
-from urllib.parse import urljoin, urlparse
+import email.utils
+import threading
 import xml.etree.ElementTree as ET
 from concurrent.futures import ThreadPoolExecutor
-import threading
-import email.utils
+from urllib.parse import urljoin, urlparse
 import pytz
+import requests
+from bs4 import BeautifulSoup
+
 
 # Global configuration variables
-MAX_DEPTH = 10
-DOMAIN = 'www.example.com'
-MAX_WORKERS = 5
-OUTPUT_FILENAME = 'sitemap.xml'
-START_URL = 'https://www.example.com/home'
-USE_TIME_FILTER = True
-TIME_FILTER_THRESHOLD = datetime(2022, 9, 20, tzinfo=pytz.UTC)
+MAX_DEPTH = 10  # Maximum depth of links to traverse
+DOMAIN = 'www.example.com'  # Only links from this domain will be included in the sitemap
+MAX_WORKERS = 5  # Maximum number of threads used for parallel processing
+OUTPUT_FILENAME = 'sitemap.xml'  # The file name of the generated sitemap
+START_URL = 'https://www.example.com/home'  # The initial URL to start crawling from
+USE_TIME_FILTER = True  # Whether to filter pages by modification time
+TIME_FILTER_THRESHOLD = datetime(2022, 9, 20, tzinfo=pytz.UTC)  # Only include pages modified after this date
 
 # Initialize XML
 urlset = ET.Element('urlset', xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
@@ -29,27 +34,31 @@ lock = threading.Lock()
 # Requests session for connection pooling
 session = requests.Session()
 
+
 def process_page(url, lastmod):
     """
-    Adds page details to the sitemap
+    Adds page details to the sitemap.
     """
-    with lock: # Protecting the critical section with a lock
+    with lock:
         url_element = ET.SubElement(urlset, 'url')
         ET.SubElement(url_element, 'loc').text = url
         ET.SubElement(url_element, 'lastmod').text = lastmod.isoformat()
         ET.SubElement(url_element, 'changefreq').text = 'daily'
         ET.SubElement(url_element, 'priority').text = '1.0'
 
-# Function to generate the sitemap
+
 def generate_sitemap(url, max_depth=MAX_DEPTH, depth=0):
+    """
+    Recursively crawls a website and generates a sitemap.
+    """
     if depth > max_depth:
         return
 
     try:
         # Fetch the content of the page
         response = session.get(url)
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
+    except requests.RequestException as error:
+        print(f"Error fetching {url}: {error}")
         return
 
     if response.status_code != 200:
@@ -77,7 +86,7 @@ def generate_sitemap(url, max_depth=MAX_DEPTH, depth=0):
         if not href.startswith('#'):
             full_url = urljoin(base_url, href)
             domain = urlparse(full_url).netloc
-            with lock: # Protecting the critical section with a lock
+            with lock:
                 if full_url not in visited_urls and domain == DOMAIN:
                     visited_urls.add(full_url)
                     links_to_process.append(full_url)
@@ -85,6 +94,7 @@ def generate_sitemap(url, max_depth=MAX_DEPTH, depth=0):
     # Use multithreading to process links
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
         executor.map(generate_sitemap, links_to_process, [max_depth] * len(links_to_process), [depth + 1] * len(links_to_process))
+
 
 # Start generating the sitemap
 generate_sitemap(START_URL)
